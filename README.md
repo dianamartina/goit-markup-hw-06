782ce0a0eee429cd8510f6775b9296947ffc9840
Diana Martina S <64376820+dianamartina@users.noreply.github.com>
2024-08-25 01:13:53 +0300

    hw-6

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# goit-markup-hw-05
+# goit-markup-hw-06
 
-https://dianamartina.github.io/goit-markup-hw-05/
+https://dianamartina.github.io/goit-markup-hw-06/
 
 HTML structure from a figma file.
 CSS customization for each element.
 Add spaces to each element and position related to other elements, using flex and margin.
 Adding form + modal form
+Customization for mobile and tablets
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const button = document.querySelector(".order-button");
 const backdrop = document.querySelector(".modal-backdrop");
+const menu = document.querySelector(".hamburger-menu");
+const modalMenu = document.querySelector(".menu-modal");
 
+// form modal open/close
 button.addEventListener("click", toggleModal);
-
 backdrop.addEventListener("click", (e) => {
   const target = e.target.closest(".close-modal-button");
 
@@ -12,6 +14,21 @@ backdrop.addEventListener("click", (e) => {
   toggleModal();
 });
 
+// menu modal open/close
+menu.addEventListener("click", toggleModalMenu);
+modalMenu.addEventListener("click", (e) => {
+  const target = e.target.closest(".close-modal-button");
+
+  if (!target) {
+    return;
+  }
+  toggleModalMenu();
+});
+
 function toggleModal() {
   backdrop.classList.toggle("is-open");
+}
+
+function toggleModalMenu() {
+  modalMenu.classList.toggle("is-open");
 }
